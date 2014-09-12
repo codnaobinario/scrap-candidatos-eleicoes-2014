9ac0018f47211b3c11d0e0ca260b6a3e2573c983
Lucas Pirola <lucaspirola@gmail.com>
2014-09-12 09:26:24 -0300

    added role of candidate to information to be imported; enhancement to better show status of import

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ var Import = {
   candidates : new Array,
 
   roles : new Array(
-    {'key' : 1,  'value' : 'presidente'         },
-    {'key' : 2,  'value' : 'vice-presidente'    },
-    {'key' : 3,  'value' : 'governador'         },
-    {'key' : 4,  'value' : 'vice-governador'    },
-    {'key' : 5,  'value' : 'senador'            },
-    {'key' : 6,  'value' : 'deputado-federal'   },
-    {'key' : 7,  'value' : 'deputado-estadual'  },
-    {'key' : 8,  'value' : 'deputado-distrital' },
-    {'key' : 9,  'value' : 'senador-1-suplente' },
-    {'key' : 10, 'value' : 'senador-2-suplente' }
+    {'key' : 1,  'value' : 'Presidente'         },
+    {'key' : 2,  'value' : 'Vice-Presidente'    },
+    {'key' : 3,  'value' : 'Governador'         },
+    {'key' : 4,  'value' : 'Vice-Governador'    },
+    {'key' : 5,  'value' : 'Senador'            },
+    {'key' : 6,  'value' : 'Deputado Federal'   },
+    {'key' : 7,  'value' : 'Deputado Estadual'  },
+    {'key' : 8,  'value' : 'Deputado Distrital' },
+    {'key' : 9,  'value' : 'Senador 1º suplente' },
+    {'key' : 10, 'value' : 'Senador 2º suplente' }
   ),
 
   base_url : 'http://divulgacand2014.tse.jus.br',
@@ -62,15 +62,14 @@ var Import = {
     var deferred     = Q.defer(),
         total_states = _.size(Import.states)-1,
         candidates   = new Array,
-        bar          = new ProgressBar('Searching states :bar', { total: total_states });
+        bar          = new ProgressBar('Procurando candidatos por estado e cargo [:bar] :percent :etas', { total: total_states*Import.roles.length });
 
     _.each(Import.states, function (uf, idx, l) {
-      bar.tick();
       _.each(Import.roles, function (cargo) {
         var url = '/divulga-cand-2014/eleicao/2014/UF/'+uf.key+'/candidatos/cargo/'+cargo.key;
         Import.call(url,
           function (err, response, body) {
-        console.log(url);
+            //console.log('Lendo candidatos de '+uf.key+', cargo '+cargo.value);
             if (err) {
               deferred.reject(err);
               throw err;
@@ -78,12 +77,13 @@ var Import = {
 
             var $ = cheerio.load(body);
             $('#tbl-candidatos tbody tr td a').each(function(i, el) {
-              candidates.push({'cand_id':el.attribs.id.slice(5),'href':el.attribs.href});
+              candidates.push({'cargo':cargo.value, 'cand_id':el.attribs.id.slice(5),'href':el.attribs.href});
             });
             if (parseInt(idx) === parseInt(total_states)) {
               Import.candidates = candidates;
               deferred.resolve(Import);
             }
+            bar.tick();
         });
       });
     });
@@ -100,12 +100,11 @@ var Import = {
   read_profile : function () {
     var deferred = Q.defer(),
       total_candidates = _.size(Import.candidates)-1,
-      bar          = new ProgressBar('Searching candidates :bar', { total: total_candidates });
+      bar          = new ProgressBar('Lendo e gravando o candidato [:bar] :percent :etas', { total: total_candidates });
 
     _.each(Import.candidates, function (profile, idx) {
       Import.call(profile.href,
         function (err, response, body) {
-          console.log(profile.href);
           if (err) {
             deferred.reject(err);
             throw err;
@@ -135,50 +134,35 @@ var Import = {
           profile.numero_protocolo = $('body > div.container > div.col-md-10 > table > tbody > tr:nth-child(10) > td:nth-child(4)').text();
           profile.limite_gastos   = $('body > div.container > div.col-md-10 > table > tbody > tr:nth-child(11) > td:nth-child(4)').text();
 
-            var candidate = new CandidateModel();
-            candidate = _.extend(candidate, profile);
+          //console.log('Gravando candidato de '+profile.naturalidade+' nome:'+profile.nome_urna);
 
-            // save the bear and check for errors
-            candidate.save(function(err) {
-            if (err) {
-                console.error(err);
-            }
-          });
-          if (parseInt(idx) === parseInt(total_candidates)) {
-            deferred.resolve(Import.candidates);
+          var candidate = new CandidateModel();
+          candidate = _.extend(candidate, profile);
+
+          candidate.save(function(err) {
+          if (err) {
+              console.error(err);
           }
-          bar.tick();
         });
+        if (parseInt(idx) === parseInt(total_candidates)) {
+          deferred.resolve(Import.candidates);
+        }
+        bar.tick();
+      });
 
       return deferred.promise;
     });
   }
 }
 
-// Q.fcall(Import.read_profile)
 Q.fcall(Import.search_states)
   .then(Import.search_candidates)
   .then(Import.read_profile)
   .catch(function (error) {
+    if (error) {
       console.error(error);
+    }
   })
   .then(function (result) {
-    console.log(result);
-    // var total_candidates = _.size(result.candidates)-1;
-    // var bar = new ProgressBar('Saving candidates :bar', { total: total_candidates });
-
-    // _.each(result.candidates, function(vv) {
-    //   var candidate = new CandidateModel();
-    //   candidate = _.extend(candidate, vv);
-
-    //   // save the bear and check for errors
-    //   candidate.save(function(err) {
-    //     if (err) {
-    //         console.error(err);
-    //     }
-    //   });
-    // });
+    // process.kill()
   });
-
-
-
